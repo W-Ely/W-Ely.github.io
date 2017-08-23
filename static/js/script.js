9d4cad07728eeb02d6f7a06e94851221730678f2
W. Ely Paysinger <paysinger@gmail.com>
2017-08-23 13:01:21 -0700

    tinkering with SPA and angular.

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,34 @@
-var app = new senna.App();
-app.setBasePath('/');
-app.addSurfaces('content');
-app.addRoutes([
-  new senna.Route('page1.html', senna.HtmlScreen),
-  new senna.Route('page2.html', senna.HtmlScreen)
-]);
+var app = angular.module('myApp', ['ngRoute']);
+
+app.config(function($routeProvider) {
+  $routeProvider
+
+  .when('/', {
+    templateUrl : 'pages/home.html',
+    controller  : 'HomeController'
+  })
+
+  .when('/blog', {
+    templateUrl : 'pages/blog.html',
+    controller  : 'BlogController'
+  })
+
+  .when('/about', {
+    templateUrl : 'pages/about.html',
+    controller  : 'AboutController'
+  })
+
+  .otherwise({redirectTo: '/'});
+});
+
+app.controller('HomeController', function($scope) {
+  $scope.message = 'Hello from HomeController';
+});
+
+app.controller('BlogController', function($scope) {
+  $scope.message = 'Hello from BlogController';
+});
+
+app.controller('AboutController', function($scope) {
+  $scope.message = 'Hello from AboutController';
+});
