138181ef812227cb80e512a99ae7bdd1223bbfd0
W. Ely Paysinger <paysinger@gmail.com>
2017-09-14 12:45:38 -0700

    some changes to structure.

--- static/js/script.js
+++ static/js/script.js
@@ -1,31 +1,21 @@
 'use strict';
 
-// Senna related
-var app = new senna.App();
-app.setBasePath('/');
-app.addRoutes([
-  {
-    path: '/index.html',
-    handler: senna.HtmlScreen
-  },
-  {
-    path: '/services.html',
-    handler: senna.HtmlScreen
-  },
-  {
-    path: '/contact.html',
-    handler: senna.HtmlScreen
-  }
-]);
-
-// Jquery Tab class manipulation
-$(document).ready(function() {
-	//jquery
-    $(location).attr('href');
-
-	//pure javascript
-	var pathname = window.location.pathname;
-
-	// to show it in an alert window
-    alert(window.location);
-});
+// // Jquery Tab class manipulation
+// $(document).ready(function() {
+// 	//jquery
+//     $(location).attr('href');
+//
+// 	//pure javascript
+// 	var pathname = window.location.pathname;
+//
+// 	// to show it in an alert window
+//     alert(window.location);
+// });
+//
+// $('body').on('mouseenter mouseleave','.dropdown',function(e){
+//   var _d = $(e.target).closest('.dropdown');
+//   _d.addClass('show');
+//   setTimeout(function(){
+//     _d[_d.is(':hover')?'addClass':'removeClass']('show');
+//   },300);
+// });
